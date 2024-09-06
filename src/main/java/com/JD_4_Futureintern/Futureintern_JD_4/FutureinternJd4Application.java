bb6c6c2ef40227f8bd3049e04e2cd1da2bcfed46
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-06 09:59:51 +0530

    Initial commit : Project created wth some dependencies lie springboot web, thymeleaf, dev tools. from start.spring.io, following MVC Architecture, Java 21,Maven, Jar

---
+++ src/main/java/com/JD_4_Futureintern/Futureintern_JD_4/FutureinternJd4Application.java
@@ -0,0 +1,13 @@
+package com.JD_4_Futureintern.Futureintern_JD_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureinternJd4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureinternJd4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/JD_4_Futureintern/Futureintern_JD_4/FutureinternJd4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JD_4_Futureintern.Futureintern_JD_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureinternJd4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
